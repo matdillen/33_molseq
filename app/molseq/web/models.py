50c5d72b8f10a359283eca6b4c36ae318691b27f
rukayaj@gmail.com <rukayaj@gmail.com>
2020-11-11 13:10:27 +0100

    Add model

--- app/molseq/web/models.py
+++ app/molseq/web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models import JSONField
+import requests
 
-# Create your models here.
+class MatchingRun(models.Model):
+    genbank_query = models.CharField(max_length=500)
+    genbank_results = JSONField()
+    gbif_query = models.CharField(max_length=500)
+    gbif_results = JSONField()
+    created = models.DateField(auto_now_add=True)
+
+    def save(self):
+
