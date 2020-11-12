45c6f503ec32c9ab03b4999eedb6147f5c96315b
Nicola <nicolawongwaiyee@gmail.com>
2020-11-12 22:14:30 +0100

    add ncbi_taxonomy_get_lineage

--- app/django/web/ena.py
+++ app/django/web/ena.py
@@ -5,6 +5,7 @@ from pygbif import occurrences
 from wikidataintegrator import wdi_core
 import pandas as pd
 import numpy as np
+from ete3 import NCBITaxa
 
 
 class ENAtoGBIF:
@@ -16,12 +17,20 @@ class ENAtoGBIF:
     base_url = "https://www.ebi.ac.uk/ena/portal/api/"
     ena_accession = None
     ena_query = None
+    gbif_query = {
+        "institutionCode" : "", 
+        "taxonKey" : ""
+    }
 
-    def __init__(self,ena_accession:list,ena_query:str):
+    ncbi = NCBITaxa()
+
+    def __init__(self,ena_accession:list,ena_query:str,gbif_query:dict):
         self.ena_accession = ena_accession  # accession candidates (i.e. from user/ PaperParser)
         self.ena_query = ena_query  # more flexible search "specimen_voucher=\"*BR)*\"", this will be placed directly in the api query string
         if not self.ena_accession == None or self.ena_query == None:
             raise Exception("Only accept either one of these: ena_accession, ena_query. Not both.")
+        if gbif_query:
+            self.gbif_query.update(gbif_query)
 
     def get_ena_results(self):
         params_d = {
@@ -30,11 +39,11 @@ class ENAtoGBIF:
             "format": "json",
             "limit": 0
         }
-        
+
         # construct query strong from list of ena_accession
         if ena_accession:
             for i,a in enumerate(ena_accession):
-                if i == 1:
+                if i == 0:
                     ena_accession = f"accession=\"{a}\""
                     continue
                 else:
@@ -50,7 +59,10 @@ class ENAtoGBIF:
         results = occurrences.search(**self.gbif_query)['results']
         return {r['gbifID']: r for r in results}
 
-    def get_wikidata_results(self, tax_ids):
+    def get_wikidata_results(self, tax_ids:list):
+
+        # TODO: if there is no match, go up to family level
+
         query_template = """
                 SELECT ?taxon ?taxonLabel ?ncbi_taxonID ?gbifid WHERE {
                   VALUES ?ncbi_taxonID {%s}
@@ -58,16 +70,31 @@ class ENAtoGBIF:
                   OPTIONAL {?taxon wdt:P846 ?gbifid .}
                   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
                 }
-                """
+                """ 
         results = {}
         for tax_ids_subset in np.array_split(list(tax_ids), 30):
             query = query_template % ('"' + '" "'.join(tax_ids_subset.tolist()) + '"')
             try:
+                # result_df.shape[0] should match ncbi_taxonID
                 result_df = wdi_core.WDFunctionsEngine.execute_sparql_query(query=query, as_dataframe=True)
+                # TODO: check which cell in column gbifid is empty, compare the df['ncbi_taxonID'] with the query listy
+                # TODO: filter the unmatch ncbi_taxonID and go up to family level
+                # query wikidata using the same query_template (should to it recursively, but can also stop if we cannot find the match order name)
                 if results == {}:
                     results = result_df
                 else:
                     results.append(result_df)
             except Exception as e:
                 print(e)
+
+        # Find the family name of them and put it to WHERE?
+
         return results.replace(np.nan, '').to_dict()
+
+    def ncbi_taxnomy_get_lineage(self,ncbi_taxonID:list):
+        lineage_ls = []
+        # http://etetoolkit.org/docs/latest/tutorial/tutorial_ncbitaxonomy.html
+        self.ncbi.update_taxonomy_database()  #  this may take long time, better to include the sqlite db (~300mb) in the image
+        for i in ncbi_taxonID:
+            lineage_ls.append(self.ncbi.get_lineage(int(i)))
+        return lineage_ls
