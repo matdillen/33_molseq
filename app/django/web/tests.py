8de87403896a64b0ca48fc7e7adbe9d99b0ded5a
Nicola <nicolawongwaiyee@gmail.com>
2020-11-13 11:10:31 +0100

    update get_wikidata_results

--- app/django/web/tests.py
+++ app/django/web/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .paperParser import paperParser
+from .ena import ENAtoGBIF
 
 # Create your tests here.
+
+# test paperParser
+sample_pdf = "static/252-Texto del artículo-603-2-10-20170418.pdf"
+p = paperParser(pdf_path=sample_pdf)
+accession_ls = p.auto_parse()
+
+# test ena
+m = ENAtoGBIF(ena_accession=accession_ls)
+m.get_ena_results()
+
+
+
