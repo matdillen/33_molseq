6b493c8e4c4037687e0a5f946f54ddcaf8b0cd4d
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2020-11-17 16:15:05 +0100

    add json export

--- bh-cleanup.R
+++ bh-cleanup.R
@@ -36,7 +36,7 @@ for (i in 1:dim(brf)[1]) {
   brf$numbers[i] = paste(nums,collapse="|")
 }
 
-coll = read_tsv("meise-coll.txt",na="")
+coll = read_tsv("data/meise-coll.txt",na="")
 
 #Add person name info to the ENA results
 #Match surnames from Meise's collector db to the specimen_voucher strings
@@ -91,7 +91,8 @@ brf$recordedByID = gsub("^\\|",
                         brf$recordedByID)
 
 #read GBIF dataset occurrence file
-dwc = read_tsv("occurrence.txt",col_types = cols(.default = "c"))
+dwc = read_tsv("occurrence.txt",
+               col_types = cols(.default = "c"))
 
 #extract numeric part from recordNumber
 dwc$num = gsub("[^0-9]","",dwc$recordNumber)
@@ -126,19 +127,25 @@ query <- 'SELECT ?taxon ?taxonLabel ?ncbi_taxonID ?gbifid WHERE {
 
 #list query results
 for (i in seq(1,dim(tax)[1],30)) {
-  subtax = paste0("\"",paste(tax$tax_id[i:(i+29)],collapse="\" \""),"\"")
+  subtax = paste0("\"",
+                  paste(tax$tax_id[i:(i+29)],
+                             collapse="\" \""),
+                  "\"")
   que = gsub("%s",
              subtax,query,fixed=T)
   if (i==1) {
     resu = querki(que)
   } else {
-    resu = rbind(resu,querki(que))
+    resu = rbind(resu,
+                 querki(que))
   }
   print(i)
 }
 
 #join into ena sequence data
-brf = left_join(brf,resu,by=c("tax_id"="ncbi_taxonID"))
+brf = left_join(brf,
+                resu,
+                by=c("tax_id"="ncbi_taxonID"))
 
 #match for 3 criteria:
 
@@ -153,15 +160,110 @@ brf = left_join(brf,resu,by=c("tax_id"="ncbi_taxonID"))
 
 brf$gbifbc = NA
 for (i in 1:dim(brf)[1]) {
-  nums = filter(brf2,accession==brf$accession[i])
-  ids = strsplit(nums$recordedByID,split="\\|")[[1]]
+  nums = filter(brf2,
+                accession==brf$accession[i])
+  ids = strsplit(nums$recordedByID,
+                 split="\\|")[[1]]
   resu = filter(dwc,
                 recordedByID%in%ids,
                 num%in%nums$numbers,
                 !is.na(recordedByID),
                 !is.na(recordNumber),
                 taxonKey%in%nums$gbifid)
-  brf$gbifbc[i] = paste(resu$catalogNumber,collapse="|")
+  brf$gbifbc[i] = paste(resu$catalogNumber,
+                        collapse="|")
 }
 
-write_tsv(brf,"br-results.txt",na="")
+write_tsv(brf,"br-results.txt",
+          na="")
+
+#generate jsons for the django app
+
+brf3 = brf
+
+brf4 = separate_rows(brf3,
+                     gbifbc,
+                     sep="\\|")
+
+#dwc is the occurrence darwin core file from GBIF
+#for this instance, it's the file for the MeiseBG herbarium
+
+gbifn = select(dwc,
+               gbifID,
+               catalogNumber,
+               scientificName,
+               countryCode,
+               recordedBy,
+               eventDate,
+               institutionCode,
+               basisOfRecord)
+
+brf5 = left_join(brf4,
+                 gbifn,
+                 by=c("gbifbc"="catalogNumber"))
+
+#render jsons
+
+#alldata for each ENA sequence (includes GBIF data)
+li = list()
+names = ""
+k=1
+for (i in 1:dim(brf3)[1]) {
+  dat = filter(brf6,accession == brf3$accession[i])
+  
+  if (dim(dat)[1]==1) {
+    li[[k]] = select(dat,-accession)
+    k = k + 1
+  }
+  
+  if (dim(dat)[1]>1) {
+    li[[k]] = vector("list",dim(dat)[1])
+    for (j in 1:dim(dat)[1]) {
+      li[[k]][[j]] = select(dat,-accession)[j,]
+    }
+    k = k + 1
+  }
+  
+  if (dim(dat)[1]!=0) {
+    names = c(names,dat$accession[1])
+  }
+}
+names = names[-1]
+names(li) = names
+
+#only ids of matches: ENA to (multiple) GBIF ids
+li2 = list()
+names = ""
+k=1
+for (i in 1:dim(brf3)[1]) {
+  dat = filter(brf6,accession == brf3$accession[i])
+  
+  if (dim(dat)[1]==1) {
+    li2[[k]] = dat$gbifID
+    k = k + 1
+  }
+  
+  if (dim(dat)[1]>1) {
+    li2[[k]] = vector("list",dim(dat)[1])
+    for (j in 1:dim(dat)[1]) {
+      li2[[k]][[j]] = dat$gbifID[j]
+    }
+    k = k + 1
+  }
+  
+  if (dim(dat)[1]!=0) {
+    names = c(names,dat$accession[1])
+  }
+}
+names = names[-1]
+names(li2) = names
+
+#stilltodo: similar json listing with all data for each GBIF id
+
+#export
+comb = toJSON(li2,pretty=T,auto_unbox = T)
+write(comb,"matchids.json")
+
+comb2 = toJSON(li,pretty=T,auto_unbox = T)
+write(comb2,"matchids-alldata.json")
+
